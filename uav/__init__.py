f825ba353686377591dfdf02f6577a3fdf4023a2
newsonjacob <newsonjacob@gmail.com>
2025-06-05 14:35:43 +0100

    Export flow variance helpers

--- uav/__init__.py
+++ uav/__init__.py
@@ -1,7 +1,7 @@
 """UAV package providing perception, navigation and interface utilities."""
 
 from .perception import OpticalFlowTracker, FlowHistory
-from .navigation import Navigator
+from .navigation import Navigator, should_fallback, FLOW_STD_MAX
 from .interface import exit_flag, start_gui
 from .utils import apply_clahe, get_yaw, get_speed, get_drone_state
 
@@ -9,6 +9,8 @@ __all__ = [
     "OpticalFlowTracker",
     "FlowHistory",
     "Navigator",
+    "should_fallback",
+    "FLOW_STD_MAX",
     "exit_flag",
     "start_gui",
     "apply_clahe",
