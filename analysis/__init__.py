b04b0d2c3bf60349c02477ea9aa5975cf496aefb
newsonjacob <newsonjacob@gmail.com>
2025-06-05 17:37:17 +0100

    Add flight review module and tests

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -2,5 +2,12 @@
 
 from .summarize_runs import summarize_log
 from .utils import retain_recent_views
+from .flight_review import parse_log, align_path, review_run
 
-__all__ = ["summarize_log", "retain_recent_views"]
+__all__ = [
+    "summarize_log",
+    "retain_recent_views",
+    "parse_log",
+    "align_path",
+    "review_run",
+]
